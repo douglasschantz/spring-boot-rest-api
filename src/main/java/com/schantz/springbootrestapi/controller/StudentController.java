5573eaf7960f32ccb14df855bce4a9248610fbb9
Douglas Schantz <schantzdouglas@gmail.com>
2023-09-01 17:46:19 -0300

    put delete

--- src/main/java/com/schantz/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/schantz/springbootrestapi/controller/StudentController.java
@@ -3,9 +3,15 @@ package com.schantz.springbootrestapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.schantz.springbootrestapi.bean.Student;
@@ -44,6 +50,36 @@ public class StudentController {
 	public Student StudentRequestVariable(@RequestParam int id, @RequestParam String name) {
 		return new Student(id, name);
 	}
+	
+	//spring boot res api that handle http post request
+	//@postpaming and @requestbody
+	@PostMapping("students/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Student createStudent(@RequestBody Student student) {
+		System.out.println(student.getId());
+		System.out.println(student.getName());
+		return student;
+	}
+	
+	//spring booot rest api that handles http put request
+	@PutMapping("students/{id}/update")
+	public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+		System.out.println(student.getName());
+		return student;
+	}
+	
+	//Spring Boot rest api tha handles http delete request
+	@DeleteMapping("students/{id}/delete")
+	public String deleteStudent(@PathVariable("id") int studentId) {
+		return "Studente deleted sucessfull";
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
