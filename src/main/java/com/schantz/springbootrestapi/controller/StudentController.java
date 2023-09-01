be44beeb2ee7422f9ada750fcfeb202c0d28ff3a
Douglas Schantz <schantzdouglas@gmail.com>
2023-09-01 17:56:44 -0300

    Response

--- src/main/java/com/schantz/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/schantz/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,20 +23,23 @@ public class StudentController {
 	
 	//http://localhost:8080/student
 	@GetMapping("student")
-	public Student getStudent() {
+	public ResponseEntity<Student> getStudent() {
 		Student student = new Student(1, "Douglas");
-		return student;
+		//return new ResponseEntity<>(student, HttpStatus.OK);
+		return ResponseEntity.ok(student);
+		
 	}
 	
 	//http://localhost:8080/students
 	@GetMapping("students")
-	public List<Student> getStudents(){
+	public  List<Student> getStudents(){
 		List<Student> students = new ArrayList<>();
 		students.add(new Student(1, "Douglas"));
 		students.add(new Student(2, "Taina"));
 		students.add(new Student(3, "Pedro"));
 		students.add(new Student(4, "Maria"));
 		return students;
+		
 		
 	}
 	
